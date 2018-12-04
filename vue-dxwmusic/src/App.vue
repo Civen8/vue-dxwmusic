2e5a35744e8475cf8c2a170d822da990067db545
dxw <15156012819@163.com>
2018-12-04 15:23:52 +0800

    now

---
+++ vue-dxwmusic/src/App.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="contain">
+    <!--头部-->
+    <div class="top">
+      <ul>
+        <li>
+          <a href="#" class="person"></a>
+        </li>
+        <li>
+          <a href="#">qq音乐</a>
+        </li>
+        <li>
+          <a href="#" class="search"></a>
+        </li>
+      </ul>
+    </div>
+    <div class="navbar">
+      <ul>
+        <li>
+          <a href="#">推荐</a>
+        </li>
+        <li>
+          <a href="#">排行榜</a>
+        </li>
+        <li>
+          <a href="#">搜索</a>
+        </li>
+      </ul>
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style lang="scss">
+html {
+  font-size: 14px;
+}
+* {
+  margin: 0px;
+  padding: 0px;
+}
+.contain {
+  width: 100%;
+  height: 100%;
+}
+.top {
+  width: 100%;
+  height: 40px;
+  ul {
+    display: flex;
+    justify-content: space-between;
+    li {
+      list-style-type: none;
+      a {
+        color: black;
+        font-size: 1.6rem;
+        text-decoration: none;
+        line-height: 40px;
+        text-align: center;
+      }
+    }
+  }
+}
+.person::before {
+  content: "\E613";
+}
+.search::before {
+  content: "\E624";
+}
+.navbar {
+  width: 100%;
+  height: 40px;
+  ul {
+    display: flex;
+    li {
+      height: 40px;
+      flex: 1;
+      list-style-type: none;
+      text-align: center;
+      line-height: 40px;
+      a {
+        color: black;
+        font-size: 1.5rem;
+        text-decoration: none;
+      }
+    }
+  }
+}
+</style>
---
+++ vue-dxwmusic/src/components/home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <mt-swipe :auto="4000">
+      <mt-swipe-item v-for="item in imgList" :key="item.id">
+        <img :src="item.img">
+      </mt-swipe-item>
+    </mt-swipe>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      imgList: [
+        { id: 1, img: require('../images/1.jpg') },
+        { id: 2, img: require('../images/2.jpg') },
+        { id: 3, img: require('../images/3.jpg') }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.mint-swipe {
+  height: 200px;
+  overflow: hidden;
+  img {
+    height: 200px;
+  }
+}
+.mint-swipe-item {
+  background-color: red;
+}
+</style>
